bf039aef08e16d13e30a2012bdee8ce0d4cfcd2f
TrucNguyen2211 <131159988+SALtruc@users.noreply.github.com>
2026-03-23 15:25:50 +0700

    Refine scale

--- src/data/roadmap.js
+++ src/data/roadmap.js
@@ -24,19 +24,19 @@ export const roadmapStageScoreLayout = {
 
 export const roadmapStageIconSlots = {
   explore: [
-    { x: 480, y: 2501, maxWidth: 560, maxHeight: 560, rotate: -3, scale: 1.8 },
-    { x: 1500, y: 3070, maxWidth: 560, maxHeight: 560, rotate: 3, scale: 2 },
-    { x: 480, y: 3850, maxWidth: 560, maxHeight: 560, rotate: -3, scale: 1.8}
+    { x: 480, y: 2501, maxWidth: 560, maxHeight: 560, rotate: -3, scale: 1.6 },
+    { x: 1500, y: 3070, maxWidth: 560, maxHeight: 560, rotate: 3, scale: 1.8 },
+    { x: 480, y: 3850, maxWidth: 560, maxHeight: 560, rotate: -3, scale: 1.6}
   ],
   develop: [
-    { x: 500, y: 4950, maxWidth: 560, maxHeight: 560, rotate: -3, scale: 1.8 },
+    { x: 500, y: 4950, maxWidth: 560, maxHeight: 560, rotate: -3, scale: 1.6 },
     { x: 1500, y: 5520, maxWidth: 560, maxHeight: 560, rotate: 3, scale: 1.8 },
-    { x: 1500, y: 6800, maxWidth: 560, maxHeight: 560, rotate: 3, scale: 1.8 }
+    { x: 1500, y: 6800, maxWidth: 560, maxHeight: 560, rotate: 3, scale: 1.6 }
   ],
   transition: [
-    { x: 480, y: 7780, maxWidth: 560, maxHeight: 560, rotate: -3, scale:2},
-    { x: 1500, y: 8470, maxWidth: 560, maxHeight: 560, rotate: 3, scale: 1.8 },
-    { x: 480, y: 9050, maxWidth: 560, maxHeight: 560, rotate: -3, scale: 2}
+    { x: 480, y: 7780, maxWidth: 560, maxHeight: 560, rotate: -3, scale: 1.8},
+    { x: 1500, y: 8470, maxWidth: 560, maxHeight: 560, rotate: 3, scale: 1.6 },
+    { x: 480, y: 9050, maxWidth: 560, maxHeight: 560, rotate: -3, scale: 1.8}
   ]
 };
 
